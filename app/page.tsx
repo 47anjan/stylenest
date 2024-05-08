a57e53b8483c329a87c1a74492e04b38366ac7db
anjankarmakar10 <anjankarmakar15@gmail.com>
2024-05-08 23:44:28 +0600

    Add link to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Container from "@/components/container";
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <Container>
+        <Link href="/latest-arrivals">Product Grid</Link>
+      </Container>
+    </main>
+  );
 }
