ba341a1d6bd19674df0b351be34654c205f4f494
anjankarmakar10 <anjankarmakar15@gmail.com>
2024-05-09 08:18:20 +0600

    change heading font

--- app/latest-arrivals/page.tsx
+++ app/latest-arrivals/page.tsx
@@ -7,7 +7,7 @@ const LatestArrivalsPage = () => {
     <Container>
       <div className="my-10 lg:my-20 xl:my-32">
         <header className="flex items-center justify-between   mb-5 md:mb-8">
-          <h2 className="text-2xl font-bold">Latest Arrivals</h2>
+          <h2 className="text-2xl md:text-3xl font-bold">Latest Arrivals</h2>
           <Link
             href="/latest-arrivals"
             className="px-4 py-2 text-sm font-medium border rounded"
