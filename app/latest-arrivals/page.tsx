ba1cce71ec87123fce6c7287177237222f220681
anjankarmakar10 <anjankarmakar15@gmail.com>
2024-05-09 08:11:02 +0600

    change responsive

--- app/latest-arrivals/page.tsx
+++ app/latest-arrivals/page.tsx
@@ -5,17 +5,19 @@ import ProductGrid from "./_components/ProductGrid";
 const LatestArrivalsPage = () => {
   return (
     <Container>
-      <header className="flex items-center justify-between my-10 lg:my-20 xl:my-32 ">
-        <h2 className="text-2xl font-bold">Latest Arrivals</h2>
-        <Link
-          href="/latest-arrivals"
-          className="px-4 py-2 text-sm font-medium border rounded"
-        >
-          View All
-        </Link>
-      </header>
+      <div className="my-10 lg:my-20 xl:my-32">
+        <header className="flex items-center justify-between   mb-5 md:mb-8">
+          <h2 className="text-2xl font-bold">Latest Arrivals</h2>
+          <Link
+            href="/latest-arrivals"
+            className="px-4 py-2 text-sm font-medium border rounded"
+          >
+            View All
+          </Link>
+        </header>
 
-      <ProductGrid />
+        <ProductGrid />
+      </div>
     </Container>
   );
 };
