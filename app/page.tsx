052cfac3521ffd94f95eea097f42283691b99dee
anjankarmakar10 <anjankarmakar15@gmail.com>
2024-05-11 12:44:47 +0600

    Remove home page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,3 @@
-import Container from "@/components/container";
-
 export default function Home() {
-  return (
-    <main>
-      <Container>
-        <div className="py-10 md:py-20">
-          <h1 className="text-lg font-bold text-slate-800">
-            <span className="text-green-600 text-xl">
-              GreatFrontEnd Projects Challenge:
-            </span>{" "}
-            A fully functional e-commerce website for a mock e-commerce platform
-          </h1>
-        </div>
-      </Container>
-    </main>
-  );
+  return <main></main>;
 }
